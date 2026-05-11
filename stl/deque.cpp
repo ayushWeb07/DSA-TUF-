e498d4e19a3372bd4ca261a9af20c5634376c828
Ayush Bommana <bommanaayush07@gmail.com>
2026-05-11 14:34:20 +0530

    deque: Implement push & traversal operation

---
+++ stl/deque.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    deque<int> dq;
+    dq.push_front(1);
+    dq.push_front(2);
+    dq.push_front(3);
+    dq.push_back(4);
+    dq.push_back(5);
+
+    for (int ele: dq) {
+        cout << ele << " ";
+    }
+
+    return 0;
+}
