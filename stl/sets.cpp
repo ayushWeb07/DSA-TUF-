4cb1f10a48de2af6e73942a9db43eed548fafe66
Ayush Bommana <bommanaayush07@gmail.com>
2026-05-11 15:00:54 +0530

    sets: Implement insertion & find operations

---
+++ stl/sets.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    set<int> s;
+
+    s.insert(1);
+    s.insert(3);
+    s.insert(5);
+    s.insert(5);
+    s.insert(3);
+    s.insert(7);
+    s.insert(3);
+    s.insert(7);
+
+    for (int ele: s) {
+        cout << ele << " ";
+    }
+
+    cout << endl;
+
+    // find for 10
+    auto it= s.find(10);
+
+    if (it == s.end()) {
+        cout << "Search failed";
+    } else {
+        cout << "Search succeeded";
+    }
+
+
+    return 0;
+}
