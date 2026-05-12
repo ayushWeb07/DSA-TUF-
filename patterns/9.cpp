9e41426766729bf7546eb1ffb8a1ec39477da293
Ayush Bommana <bommanaayush07@gmail.com>
2026-05-13 04:57:33 +0530

    patterns: Solve 6-10 problems

---
+++ patterns/9.cpp
@@ -0,0 +1,43 @@
+    //    *
+    //   ***
+    //  *****
+    // *******
+    // *******
+    //  *****
+    //   ***
+    //    *
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+   void pattern9(int n) {
+      for(int i=1; i<=n; i++) {
+
+         for(int j=1; j<= n-i; j++) {
+            cout << " ";
+         }
+
+         for(int j=1; j<= (2 * i - 1); j++) {
+            cout << "*";
+         }
+
+         cout << endl;
+      }
+
+
+      for(int i=n; i>=1; i--) {
+
+         for(int j=1; j<= n-i; j++) {
+            cout << " ";
+         }
+
+         for(int j=1; j<= (2 * i - 1); j++) {
+            cout << "*";
+         }
+
+         cout << endl;
+      }
+   }
+};
