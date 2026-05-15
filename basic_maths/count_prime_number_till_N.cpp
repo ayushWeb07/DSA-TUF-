a07769de926e3061e0f798aa17362cb0866df5ed
Ayush Bommana <bommanaayush07@gmail.com>
2026-05-15 11:58:53 +0530

    basic_maths: Solve prime number problems

---
+++ basic_maths/count_prime_number_till_N.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// approach: TC - O(n * sqrt(n)) | SC - O(1)
+class Solution {
+public:
+    bool isPrime(int n) {
+
+        if(n == 1) return false;
+
+        //your code goes here
+        for(int i=2; i*i<=n; i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    int primeUptoN(int n) {
+        int c= 0;
+
+        for(int i=2; i<=n; i++) {
+            if(isPrime(i)) {
+                c++;
+            }
+        }
+
+        return c;
+    }
+};
