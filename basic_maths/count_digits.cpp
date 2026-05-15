8f6192f01da95b13c580805917d8c3e4aab0c8be
Ayush Bommana <bommanaayush07@gmail.com>
2026-05-15 10:24:35 +0530

    basic_maths: Solve count digits & odd tigits

---
+++ basic_maths/count_digits.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// approach 1: O(log10(n))
+class Solution1 {
+public:
+    int countDigit(int n) {
+        return to_string(n).size();
+    }
+};
+
+// approach 2: O(log10(n))
+class Solution2 {
+public:
+    int count(int n) {
+        if(n <= 0) {
+            return 0;
+        }
+
+        return 1 + count(n / 10);
+    }
+
+    int countDigit(int n) {
+        if(n < 10) {
+            return 1;
+        }
+
+        return count(n);
+    }
+};
---
+++ basic_maths/count_odd_digits.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// approach 1: O(log10(n))
+class Solution1 {
+public:
+    int countOddDigit(int n) {
+        int c= 0;
+
+        while(n > 0) {
+            int d= n % 10;
+
+            if(d % 2 != 0) {
+                c++;
+            }
+
+            n/= 10;
+        }
+
+        return c;
+    }
+};
+
+// approach 2: O(log10(n))
+class Solution2 {
+public:
+    int count(int n) {
+        if(n <= 0) {
+            return 0;
+        }
+
+        // last digit is even
+        if((n % 10) % 2 == 0) {
+            return count(n / 10);
+        }
+
+        // last digit is odd
+        else {
+            return 1 + count(n / 10);
+        }
+    }
+
+    int countOddDigit(int n) {
+        return count(n);
+    }
+};
