50260510b547c16bb0006c2e00808d5aeba9e214
Ayush Bommana <bommanaayush07@gmail.com>
2026-05-19 00:55:04 +0530

    basic_string: Solve largest odd number in string by optimal approach

--- basic_string/largest_odd_number_in_string.cpp
+++ basic_string/largest_odd_number_in_string.cpp
@@ -1,4 +1,9 @@
-class Solution{	
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+// approach 1: Bruteforce - TC -> O(n^3) | SC -> O(1)
+class Solution1{
 public:	
     string largeOddNum(string& s){
         // remove leading 0s
@@ -24,5 +29,37 @@ public:
         }
 
         return "";
+    }
+};
+
+// approach 2: Optimal - TC -> O(n) | SC -> O(k) where k is the length of the substring
+class Solution2{
+public:
+    string largeOddNum(string& s){
+
+        int len= s.size();
+
+        // find the index of the first non zero element
+        int st= 0;
+
+        while(st < len && s[st] == '0') {
+            st++;
+        }
+
+        // find the index of first odd element from right
+        int end= -1;
+
+        for(int i=len-1; i>=0; i--) {
+            char ch= s[i];
+            int chNum= ch - '0';
+
+            if(chNum % 2 != 0) {
+                end= i;
+                break;
+            }
+        }
+
+        // return the substring
+        return end == -1 ? "" : s.substr(st, end - st + 1);
     }
 };
