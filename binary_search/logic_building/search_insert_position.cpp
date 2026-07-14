77d5821e1c35e1d680d0d11221e19131ae6986bd
Ayush Bommana <bommanaayush07@gmail.com>
2026-07-14 14:41:41 +0530

    binary_search: Search insert position in the array

---
+++ binary_search/logic_building/search_insert_position.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// TC - O(log n) | SC - O(1)
+class Solution {
+public:
+    int searchInsert(vector<int> &nums, int target)  {
+        int size= nums.size();
+        int st= 0, end= size - 1;
+        int ans= size;
+        int mid;
+
+        while (st <= end) {
+            mid= (st + end) / 2;
+
+            if(nums[mid] >= target) {
+                ans= mid;
+                end= mid - 1;
+            }
+
+            else {
+                st= mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
